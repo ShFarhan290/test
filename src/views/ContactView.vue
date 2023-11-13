38d0bcfa077628bd142d5c89733f961dc6bfa263
ShFarhan290 <mdhachan1950@gmail.com>
2023-11-13 21:34:43 +0600

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <h1>This Is The Contact Section</h1>
+    </div>
+</template>
---
+++ src/views/ServiceView.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <h1>There Is The Service Section</h1>
+    </div>
+</template>
